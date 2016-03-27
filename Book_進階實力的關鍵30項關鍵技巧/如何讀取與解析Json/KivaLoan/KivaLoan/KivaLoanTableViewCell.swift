0358700867774e60eae9fa7761f6eb612dad2fb4
rocooshiang <rocoo.shaing@gmail.com>
2016-03-27 18:13:54 +0800

    Add new record

---
+++ Book_進階實力的關鍵30項關鍵技巧/如何讀取與解析Json/KivaLoan/KivaLoan/KivaLoanTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  KivaLoanTableViewCell.swift
+//  KivaLoan
+//
+//  Created by Simon Ng on 20/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class KivaLoanTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel:UILabel!
+    @IBOutlet weak var countryLabel:UILabel!
+    @IBOutlet weak var useLabel:UILabel!
+    @IBOutlet weak var amountLabel:UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Book_進階實力的關鍵30項關鍵技巧/如何讀取與解析Json/KivaLoan/KivaLoan/KivaLoanTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  KivaLoanTableViewController.swift
+//  KivaLoan
+//
+//  Created by Simon Ng on 20/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class KivaLoanTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return 0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! KivaLoanTableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
