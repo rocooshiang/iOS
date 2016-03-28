b6caf6f9e457042cf8ee5fbbbe7d01d943c55805
rocooshiang <rocoo.shaing@gmail.com>
2016-03-28 12:00:00 +0800

    Add new project

--- Book_進階實力的關鍵30項關鍵技巧/如何讀取與解析Json/KivaLoan/KivaLoan/KivaLoanTableViewController.swift
+++ Book_進階實力的關鍵30項關鍵技巧/如何讀取與解析Json/KivaLoan/KivaLoan/KivaLoanTableViewController.swift
@@ -9,52 +9,105 @@
 import UIKit
 
 class KivaLoanTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // Return the number of sections.
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Return the number of rows in the section.
-        return 0
-    }
-
+  
+  let kivaLoadURL = "http://api.kivaws.org/v1/loans/newest.json"
+  var loans = [Loan]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+      getLatestLoans()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! KivaLoanTableViewCell
-
-        // Configure the cell...
-
-        return cell
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    print("Loans count: \(loans.count)")
+    return loans.count
+  }
+  
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! KivaLoanTableViewCell
+    
+    cell.nameLabel.text = loans[indexPath.row].name
+    cell.countryLabel.text = loans[indexPath.row].country
+    cell.useLabel.text = loans[indexPath.row].use
+    cell.amountLabel.text = "$\(loans[indexPath.row].amount)"
+    
+    
+    return cell
+  }
+  
+  func getLatestLoans(){
+    let request = NSURLRequest(URL: NSURL(string: kivaLoadURL)!)
+    let urlSession = NSURLSession.sharedSession()
+    
+    let task = urlSession.dataTaskWithRequest(request, completionHandler: { (data, response , error) -> Void in
+      
+      if error != nil{
+        print(error?.localizedDescription)
+      }
+      
+      // 解析Json資料
+      self.loans = self.parseJsonData(data!)
+      
+      // 重新載入表格視圖
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        self.tableView.reloadData()
+      })
+      
+    })
+    
+    task.resume()
+  }
+  
+  func parseJsonData(data: NSData) -> [Loan]{
+    var loans = [Loan]()
+    
+    do{
+      let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as? NSDictionary
+      
+      let jsonLoans = jsonResult?["loans"] as! [AnyObject]
+      
+      for jsonLoan in jsonLoans{
+        let loan = Loan()
+        loan.name = jsonLoan["name"] as! String
+        loan.amount = jsonLoan["loan_amount"] as! Int
+        loan.use = jsonLoan["use"] as! String
+        
+        let location = jsonLoan["location"] as! [String: AnyObject]
+        loan.country = location["country"] as! String
+        
+        loans.append(loan)
+      }
+      
+    }catch let error as NSError{
+      print("Error in parseJson: \(error.localizedDescription)")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    return loans
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using [segue destinationViewController].
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
 }
---
+++ Book_進階實力的關鍵30項關鍵技巧/如何讀取與解析Json/KivaLoan/KivaLoan/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Geosat-RD01 on 2016/3/28.
+//  Copyright © 2016年 AppCoda. All rights reserved.
+//
+
+import Foundation
+class Loan {
+  var name: String = ""
+  var country: String = ""
+  var use: String = ""
+  var amount: Int = 0
+}
