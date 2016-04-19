6c62eb3041c0a6dda82764d29db2c5e49b8df3b7
rocooshiang <rocoo.shaing@gmail.com>
2016-04-19 16:16:46 +0800

    Add new project named URL_Scheme

---
+++ Book_進階實力的關鍵30項關鍵技巧/URL_Scheme運用/QRReaderDemo/QRReaderDemo/ViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  QRReaderDemo
+//
+//  Created by Simon Ng on 23/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var messageLabel:UILabel!
+  
+  var captureSession:AVCaptureSession?
+  var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+  var qrCodeFrameView:UIView?
+  
+  // Added to support different barcodes
+  let supportedBarCodes = [AVMetadataObjectTypeQRCode, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeAztecCode]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+    // as the media type parameter.
+    let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+    
+    do {
+      // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+      let input = try AVCaptureDeviceInput(device: captureDevice)
+      
+      // Initialize the captureSession object.
+      captureSession = AVCaptureSession()
+      // Set the input device on the capture session.
+      captureSession?.addInput(input)
+      
+      // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+      let captureMetadataOutput = AVCaptureMetadataOutput()
+      captureSession?.addOutput(captureMetadataOutput)
+      
+      // Set delegate and use the default dispatch queue to execute the call back
+      captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+      
+      // Detect all the supported bar code
+      captureMetadataOutput.metadataObjectTypes = supportedBarCodes
+      
+      // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+      videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+      videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+      videoPreviewLayer?.frame = view.layer.bounds
+      view.layer.addSublayer(videoPreviewLayer!)
+      
+      // Start video capture
+      captureSession?.startRunning()
+      
+      // Move the message label to the top view
+      view.bringSubviewToFront(messageLabel)
+      
+      // Initialize QR Code Frame to highlight the QR code
+      qrCodeFrameView = UIView()
+      
+      if let qrCodeFrameView = qrCodeFrameView {
+        qrCodeFrameView.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView)
+        view.bringSubviewToFront(qrCodeFrameView)
+      }
+      
+    } catch {
+      print(error)
+      return
+    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func launchApp(decodedURL: String){
+    
+    let alertPrompt = UIAlertController(title: "打開應用程式", message: "你即將前往\(decodedURL)", preferredStyle: .ActionSheet)
+    let confirmAction = UIAlertAction(title: "確認", style: .Default) { (action) -> Void in
+      if let url = NSURL(string: decodedURL){
+        if UIApplication.sharedApplication().canOpenURL(url){
+          UIApplication.sharedApplication().openURL(url)
+        }
+      }
+    }
+    
+    let cancelAction = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
+    alertPrompt.addAction(confirmAction)
+    alertPrompt.addAction(cancelAction)
+    self.presentViewController(alertPrompt, animated: true, completion: nil)
+    
+  }
+  
+}
+
+extension ViewController : AVCaptureMetadataOutputObjectsDelegate{
+  func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+    
+    // Check if the metadataObjects array is not nil and it contains at least one object.
+    if metadataObjects == nil || metadataObjects.count == 0 {
+      qrCodeFrameView?.frame = CGRectZero
+      messageLabel.text = "No barcode/QR code is detected"
+      return
+    }
+    
+    // Get the metadata object.
+    let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+
+    if supportedBarCodes.contains(metadataObj.type) {
+
+      let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj)
+      qrCodeFrameView?.frame = barCodeObject!.bounds
+      
+      if metadataObj.stringValue != nil {
+        messageLabel.text = metadataObj.stringValue
+        launchApp(metadataObj.stringValue)
+      }
+    }
+    
+  }
+  
+}
+
