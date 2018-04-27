c48f659eb14d7e175dfdb621bec099f9d425cd7f
Rocoo Chuang <rocoo.shiang@gmail.com>
2018-04-27 14:13:28 +0800

    remove legacy file.

--- Net_Tutorial/ProtocolREST-UnitTest(Swift 3)/ProtocolREST/ViewController.swift
+++ Net_Tutorial/ProtocolREST-UnitTest(Swift 3)/ProtocolREST/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
 /*** disable json format ***/
-//    executeGet()
+    executeGet()
 //    executePost()
     
 /*** enable json format ***/
